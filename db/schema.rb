a2da248545f2041f0f6a9d9d21ee7b45a68f06c8
Mathilde Robert <m.robert@skema.edu>
2020-09-22 13:11:45 +0200

    delete santum

--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,6 @@ ActiveRecord::Schema.define(version: 2020_09_22_110246) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "santa", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
