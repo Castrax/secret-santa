62839b671ce2ae2e412fb56b34ab5c5d12e7b9eb
Mathilde Robert <m.robert@skema.edu>
2020-09-22 13:03:05 +0200

    add event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_105607) do
+ActiveRecord::Schema.define(version: 2020_09_22_110246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "date"
+    t.integer "max_amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "santa", force: :cascade do |t|
     t.string "name"
